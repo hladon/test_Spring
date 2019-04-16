a4787dc0d002c2535325b40733a39143d54ed548
Vitaliy <hladon@meta.ua>
2019-04-16 19:04:21 +0300

    task 2

--- src/main/java/com/ItemController.java
+++ src/main/java/com/ItemController.java
@@ -1,11 +1,12 @@
 package com;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.io.InputStream;
 
 
 @Controller
@@ -18,14 +19,31 @@ public class ItemController  {
         return itemService.read(id);
     }
 
+
+
     @PostMapping( value = "/",produces = "text/plain",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String doPost(@RequestBody Item item) {
+    public @ResponseBody String doPost(InputStream dataStream) {
+        ObjectMapper mapper=new ObjectMapper();
+        Item item=null;
+        try {
+            item=mapper.readValue(dataStream,Item.class);
+        }catch (Exception e){
+            System.err.println("Wrong input data");
+        }
+
         itemService.create(item);
         return "Save is done";
     }
 
     @PutMapping( value = "/",produces = "text/plain",consumes = MediaType.APPLICATION_JSON_VALUE )
-    public @ResponseBody String doPut(@RequestBody Item item) {
+    public @ResponseBody String doPut(InputStream dataStream) {
+        ObjectMapper mapper=new ObjectMapper();
+        Item item=null;
+        try {
+            item=mapper.readValue(dataStream,Item.class);
+        }catch (Exception e){
+            System.err.println("Wrong input data");
+        }
         itemService.update(item);
         return "Update is done";
     }
